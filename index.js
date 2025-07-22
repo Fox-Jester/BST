741ddea4a0f0cf05ff211b8e41393d4e76e61d92
William Marshall <willmarshall2424@gmail.com>
2025-07-22 17:17:31 -0400

    created set

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ class HashMap{
     constructor(){
         this.loadFactor = 1
         this.capacity = 16
+        this.map = new Array(this.capacity)
+        
     }
 
     hash(key){
         let hashCode = 0
         const primeNumber = 31;
         for(let i = 0; i < key.length; i++){
-            hashCode = primeNumber * hashCode + key.charCodeAt(i)
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity
         }
         
         return hashCode;
+    }
 
-
+    set(key, value){
+        const hashcode = this.hash(key);
+        this.map[hashcode] = value
+        console.log(this.map)
     }
 }
 
+const HM = new HashMap()
 
+HM.set("honkydonky", "wah");
+HM.set("honkydonky", "shuba");
 
 
